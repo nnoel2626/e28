571262b78a07a2abd56820d5fbf946b5df7cf0db
Norcius Noel <nnoel@fas.harvard.edu>
2019-09-22 13:12:58 -0400

    first commit for game assignment

--- game/js/wordGuess.js
+++ game/js/wordGuess.js
@@ -1,128 +1,114 @@
+ //create an array of words
+ var wordsArray = ["rabbit", "allows",
+     "snail", "airport", "carrots", "duplex", "cigars", "river", "airbus"
+ ]
+
  //Global variables//
+ const maxGuess = 10
+ var guessedLetters = []
+ var wordGuess = []
+ var word
+ var numGuess
+ var wins = 0
 
- var word = "";
- var wordGuess = [];
- var wrongGuess = [];
- var guessBomb = 0;
- var winCount = 1;
- var guess = "";
 
- function wordsArray() {
-     //create an array of words
-     var randomWords = ["rabbit", "allows",
-         "snail", "airport", "carrots", "duplex", "cigars", "river", "airbus"
-     ]
-     var raNum = Math.floor(Math.random() * 9);
-     return randomWords[raNum]
- }
- //When the enter key is pressed a new word is slected and the Current Word section is replaced with dashes
- window.onload = function () {
-     //randomWord = [];
-     start();
-     //wordStart();
- };
-
- function wordStart() {
-     var wordLength = word.length;
-     var wordL_ = "";
-     var count = wordLength;
-     while (count > 0) {
-         wordGuess.push(" _ ");
-         count -= 1;
-     }
- }
-
- function winCountFunc() {
-     var num = 0;
-     var lettUsed = "";
-     var count = word.length;
-     while (count > 0) {
-         if (lettUsed.includes(word[count - 1])) {} else {
-             num += 1;
-             lettUsed += word[count - 1];
-         }
-         count -= 1;
-     }
-     return num;
- }
- //function to start the game when load browser
  function start() {
-     word = wordsArray();
-     console.log(word);
+     numGuess = maxGuess
 
-     winCount = winCountFunc();
-     guessBomb = word.length + 4;
-     console.log(word);
+     // Get a new word
+     word = wordsArray[Math.floor(Math.random() * wordsArray.length)].toUpperCase()
+     console.log(word)
 
-     //document.getElementById('mainGame').style.display = 'block';
-     document.getElementById("question").innerHTML = "Enter your first guess";
+     // Reset word arrays
+     guessedLetters = []
+     wordGuess = []
 
-     wordStart();
+     // Reset the guessed word
+     for (var i = 0, j = word.length; i < j; i++) {
+         // Put a space instead of an underscore between multi word "words"
+         if (word[i] === " ") {
+             wordGuess.push(" ")
+         } else {
+             wordGuess.push("_")
+         }
+     }
+     //Display or hide different Div's on the page
+     document.getElementById('youWin').style.display = 'none';
+     document.getElementById('youLose').style.display = 'none';
+     document.getElementById('startButton').style.display = 'none';
+     document.getElementById('mainGame').style.display = 'block';
 
-     document.getElementById('RRguess').style.display = 'block';
-     document.getElementById("rightGuess").innerHTML = "word progress: " + wordGuess;
-     document.getElementById("wrongGuess").innerHTML = "Wrong guesses: " + wrongGuess;
-     document.getElementById("guessesLeft").innerHTML = "Guesses remaining: " + guessBomb;
+     // Update the Display
+     updateDisplay()
  }
 
- function enterGuess() {
-     var lett = document.getElementById("guess").value;
-     document.getElementById("guess").value = "";
-     console.log(lett);
 
-     if (lett.length === 1) {
-         var rightOnot = isRightOnot(lett);
-         if (rightOnot == true) {
-             NewCW(lett);
-         } else {
-             if (!wrongGuess.includes(lett)) {
-                 console.log("wrongGuess");
-                 wrongGuess.push(lett);
+
+ function updateDisplay() {
+     document.getElementById("totalWins").innerText = wins
+     document.getElementById("currentWord").innerText = wordGuess.join("")
+     document.getElementById("remainingGuesses").innerText = numGuess
+     document.getElementById("guessedLetters").innerText = guessedLetters.join(" ")
+ }
+
+
+
+ // Check in keypressed is between A-Z or a-z
+ function isAlpha(ch) {
+     return /^[A-Z]$/i.test(ch);
+ }
+
+
+
+ // Once the HTML document has been completely loaded, Listen for keyboard Events
+ document.addEventListener("DOMContentLoaded", function () {
+     // Wait for key press
+     document.onkeypress = function (event) {
+         // Make sure key pressed is an alpha character
+         if (isAlpha(event.key)) {
+             checkForLetter(event.key.toUpperCase())
+         }
+     }
+
+
+     // Check if letter is in word & process
+     function checkForLetter(letter) {
+         var foundLetter = false
+         // Search string for letter
+         for (var i = 0, j = word.length; i < j; i++) {
+             if (letter === word[i]) {
+                 wordGuess[i] = letter
+                 foundLetter = true
+                 // If guessing word matches random word
+                 if (wordGuess.join("") === word) {
+                     // Increment # of wins
+                     wins++
+                     updateDisplay()
+                     document.getElementById('youWin').style.display = 'block';
+
+                 }
              }
-             guessBomb -= 1;
          }
-     } else if (lett.length < 1) {
-
-     }
-     // else {
-     //     guessBomb -= 1;
-     // }
-
-     if (guessBomb <= 0) {
-         document.getElementById('youLose').style.display = 'block';
-         document.getElementById("correctWordWas").innerHTML = "The correct word was " + word;
-     }
-     if (winCount <= 0) {
-         document.getElementById('youWin').style.display = 'block';
-     }
-     document.getElementById("rightGuess").innerHTML = "word progress: " + wordGuess;
-     document.getElementById("wrongGuess").innerHTML = "Wrong guesses: " + wrongGuess;
-     document.getElementById("guessesLeft").innerHTML = "Guesses remaining: " + guessBomb;
- }
 
 
- //his function check if the Random word
- //contains the guessed letter return the letter
 
- function isRightOnot(a) {
-     var n = word.includes(a);
-     return n;
- }
-
- function NewCW(letter) {
-     var count = 0;
-     winCount -= 1
-     while (count <= word.length - 1) {
-         if (letter === word[count]) {
-             if (wordGuess[count] === letter) {} else {}
-             wordGuess[count] = letter;
-             count += 1;
-         } else {
-             count += 1;
+         if (!foundLetter) {
+             //  incorrectSound.play()
+             // Check if inccorrect guess is already on the list
+             if (!guessedLetters.includes(letter)) {
+                 // Add incorrect letter to guessed letter list
+                 guessedLetters.push(letter)
+                 // Decrement the number of remaining guesses
+                 numGuess--
+             }
+             if (numGuess === 0) {
+                 // Display word and display losing message
+                 wordGuess = word.split()
+                 document.getElementById('youLose').style.display = 'block';
+             }
          }
+
+         updateDisplay()
      }
- }
- //This function will reset the game
- function restart() {
-     location.reload();
- }
+
+ })
